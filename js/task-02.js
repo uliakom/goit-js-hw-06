2a6fbdc29413d9fb4de0c9654f65f23302aaea30
Uliana Komarynska <uliakom@gmail.com>
2022-04-12 23:00:22 +0300

    1st task

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,19 @@
+
+
+//Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul.ingredients.
+
+
+function createList(items) {
+  const listing = document.createElement('li');
+  // const elementToAdd = listing.classList.add('item');
+  // console.log(elementToAdd);
+  const newArray = items.map((item) => listing.textContent=`${item}` )
+  return newArray;
+}
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +22,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+console.log(createList(ingredients));
+
+
+
+
+// const addedList = ingredients.map((item, i) =>
+//   document.createAttribute('li').textContent = `${item[i]}`
+// )
+//   console.log(addedList);
